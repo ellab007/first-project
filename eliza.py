108a7c84b173ce656e22751aba99f45ce407c56c
ellab007 <91328189+ellab007@users.noreply.github.com>
2021-09-24 20:00:42 +0600

    этот код камен ,ножница бумага

---
+++ eliza.py
@@ -0,0 +1,599 @@
+#
+# # count = 0
+# # ages = [12,14,16,20,21]
+# # while count < len(ages):
+# #     if ages[count] >= 18:
+# #         print("все нормально")
+# #     else:
+# #         print("нельзя")
+# #     count = count + 1
+#
+#
+# # count = 0
+# # ages = [12,48,9,16,21]
+# # while count < len(ages):
+# #     if ages[count] >= 15:
+# #         print("ok")
+# #     else:
+# #         print("no")
+# #         count = count + 1
+#
+# # r = "pithon самый лучший язык программирования"
+# # print( r.title())
+#
+# # print("berdishova" + " eliza " * 12)
+#
+# #
+# # candy = 12
+# # while candy > 0:
+# #     print("lol")
+# #     candy = candy - 1
+#
+#
+# # l = "lol"
+# #
+# # print( l.title())
+#
+# # print("lol" + "xo " * 45)
+#
+# # for i in 'jhon':
+# #     if i == '0':
+# #         pass
+# #     print(i, end=",")
+# #
+# # a = 4
+# # b = int(input())
+# #
+# # for ip in range(25):
+# #   num = a + b
+# # print(num)
+#
+# # num = 5
+# # while num > 0:
+# #     print(num, end="")
+# #     num = num- 1
+#
+#
+# # candy = 20
+# # while candy > 0:
+# #     print("20")
+# #     candy = candy - 1
+#
+# # n = 1
+# #
+# # while n < 20:
+# #     print(n)
+# #     n += 1
+# #
+# # n = 1
+# #
+# # while n < 21:
+# #     print(n)
+# #     n += 1
+#
+# #
+# # text = "Hey привет Привет"
+# # print(text.lower())
+#
+# # text = "NEY"
+# # print(text.capitalize())
+# # #
+# #
+# # text = "123 123 123"
+# #
+# # print(text.count("2"))
+#
+# # text = "hey,hey,ney,ney,ney"
+# #
+# # result = text.rfind("h")
+# # print.fast
+#
+# # neme = input("ведите имя")
+# # # print('''hello
+# # #      hey
+# # #      hey
+# # #      hey
+# # #      alex
+#
+# # num1 =""
+# # num2 =""
+# # command = ""
+# # while True:
+# #     command = input("введи команду:")
+# #     my_list = ["add", "sub", "div", "multi"]
+# #     if command in my_list:
+# #       if command == "add":
+# #           num1 = int(input("введите число 1:"))
+# #           num2 = int(input("введите число 2:"))
+# #           result = num1 + num2
+# #           print("ответ: ",result)
+# #
+# #       elif command == "sub":
+# #          result = num1 - num2
+# #          print("ответ: ", result)
+# #
+# #       elif command == "add":
+# #          result = num1 + num2
+# #          print("ответ: ", result)
+# #
+# #       elif command == "div":
+# #          result = num1 / num2
+# #          print("ответ: ", result)
+# #
+# #       elif command == "multi":
+# #         result = num1 * num2
+# #         print("ответ: ", result)
+# #     else:
+# #         print("конец")
+# #         break
+#
+#
+# # num = 5
+# # log = True
+# # print(num + log)
+#
+# # num = 4
+# # log = False
+# # print(num + log)
+#
+# #
+# # text = "hello world"
+# # print(text.index("d"))
+#
+# # text = "hello world"
+# # print(len(text))
+# #
+# # text = "hello world"
+# # print(text[2:5])
+# #
+# #
+# # text = "hello"
+# # print("текст: ", text[-1] )
+# #
+#
+# # text = "hello"
+# # print(text)
+# # text(0) = "a"
+# # print(text)
+# #
+# # my_tuple = (1,2,3)
+# # print(my_tuple[0])
+#
+# # my_set = ("eliza","aidana","kerez","alex","ali")
+# # print(my_set)
+# # print(my_set[0])
+#
+# # array = ("hello")
+# # print(array)-
+# # #
+# # arrey1 = пр.arrey([45,14,45,42])
+# # #
+# #
+# # print(arrey1)
+# # print(type)
+# # countries = ["кыргызстан,россия,корея,китай"]
+# # del countries[0]
+# # countries.append("япония")
+# # for element in countries:
+# #     print(element, len(countries[-]) )
+#
+#
+# # my_str = "python"
+# # my_str = list(my_str)
+# # print(my_str)
+#
+# # my_str = "python"
+# # my_str = list(my_str)
+# #
+# # print(type(my_str))
+#
+# # ney = "privet"
+# # ney = list(ney)
+# # print(ney)
+# #
+# #
+# # ney = list()
+# # print(ney)
+#
+# # ney = "privet"
+# # ney = list(ney)
+# # print(ney)
+# #
+# # ney = "privet"
+# # print(list(ney))
+#
+#
+# # list = [1111,4,5,8,6,4]
+# # del list[0]
+# # print(list)
+# #
+# # list =[1,5,8,2,5,5]
+# # num = list.pop(1)
+# # print(list)
+# # print(num)
+# #
+# #
+# # list = [5,21,8,1,4]
+# # list.remove(1)
+# # print(list)
+# #
+# # list = [1,5,4,8,2,5,2,1]
+# # list.sort()
+# # print(list)
+# #
+# #
+# # list = [1,5,8,2,2]
+# # list.reverse()
+# # print(list)
+#
+# #
+# # count = 1
+# # while count < 11:
+# #     print("число:", count)
+# #     count = count + 1
+# #  #
+# # my_list =[]
+# # for element in range(21):
+# #   my_list.append(element)
+# #
+# # list1 = []
+# # list2 = []
+# # for elem in list1:
+# #     if elem % 2 == 0:
+# #       list1.append(elem)
+# #     else:
+# #       list2.append(elem)
+# # print(list1)
+# # print(list2)
+#
+# # s = 0
+# # for elem in range(0,21):
+# #     s = s + elem
+# #     print("шаг", elem)
+# #     print("сумма",s)
+# # print("финалнная сумма:", s)
+#
+#
+# # count = 1
+# # while count < 11:
+# #     print("число", count)
+# #     count = count + 1
+#
+# # for elem in range(0,21):
+# #     print(elem)
+#
+# # count = 0
+# # word = "code"
+# # while count < (len(word)):
+# #     print(word[count])
+# #     count = count + 1
+#
+# #
+# # my_dict = (1:"ella", 2:"eliza", 3:"hey")
+# # print(my_dict[1])
+#
+#
+# #
+# # x = input("Введите: ")
+# # x = int(x)
+# # if x > 0:
+# #     print(True)
+# # elif x<0:
+# #     print(False)
+#
+#
+# #
+# #
+# # x = input("веедите:" ).split()
+# # print(x)
+# # num1 = int(x[0])
+# # num2 = int(x[1])
+# # print(num1 + num2)
+#
+#
+#
+# # slovo = list(input("введите число:"))
+# # slovo.reverse()
+# # print(slovo)
+#
+#
+# # my_list = [1,2,3],[2,5,2]
+# # print(my_list[0])
+# # print(my_list[1])
+# #
+# # name =input("введите имя:")
+# # age =input("ваш возраст:")
+# # name ="имя:"+name
+# # age ="возраст:" + age
+# # print(name)
+# # print(age)
+#
+#
+# # a=12
+# # b=15
+# # v=(a+b)
+# # print(v)
+# #
+# # a=input("введите сколка вам лет:")
+# # b =10
+#
+# # print("вам осталос жыть где_то:"+str(b))
+#
+#
+# #
+# # sun = input('введите 1 если пагода сол0нечно и 2 если пасмурная:')
+# # if sun== '1':
+# #     d='нужно загорать'
+# # else:
+# #     d = 'загорат не выйдет'
+# # print(d)
+#
+#
+# # if sun == True and season == 'лето':
+# #     emotion = 'ура лето!'
+# #     print(emotion)
+# #
+# #
+# # str = 'abc'
+# # if str != '':
+# #     print("hey")
+# # if str:
+# #     print("hello")
+#
+# # a =12
+# # b =13
+# # c = a+b
+# # print(c)
+#
+#
+#
+#  # count = 1
+#  # while count < 11:
+#  #     print("число:", count)
+#  #     count = count + 1
+#
+#
+# # a = 0
+# # list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # for element in list1:
+# #     a=a+element
+# # print(a)
+# #
+#
+#
+#
+#
+# # my_dict = {"имя":"Элиза",
+# #           "урок": ["чтения","история","физкультура"],
+# #          "песня": ["во сне","колыбельная"],
+# #           "шоколад": "все"}
+# # print(my_dict)
+#
+# # list1 = ["Alex", "Maks", "Iliyaz", "Orhan", "Urmat", "Tilek", "Ali", "Eldiyar"]
+# # for element in list1:
+# #    if len(element)>4:
+# #      print(element)
+# #
+#
+# # list1 = [1, 2, 4, 6, 7, 8, 9, 10]
+# # list2 = [0, 3, 5, 6, 12 ,8, 11, 10]
+# # for element in list1:
+# #     for elem in list2:
+# #         if element == elem:
+# #             print(element)
+# #
+#
+#
+# # list1 = ["eliza","ella","asema","ermek"]
+# # for element in list1:
+# #     if len(element)<5:
+# #         print(element)
+#
+#
+#
+# # list1 =[4,1,5,1,7,6,15]
+# # list2 =[45,8,5,14,7,6,15]
+# # for element in list1:
+# #     for elem in list2:
+# #         if element ==elem:
+# #             print(element)
+#
+#
+#
+#
+#
+#
+#
+# #
+# # def adder(*num):
+# #     summmery = 0
+# #
+#
+# # import sys
+# # randomList = ['a',0,2]
+# # for element in randomList:
+# #     try:
+# #         print("элемент:", element)
+# #         print(1/element)
+# #         break
+# #     except:
+# #          print("ошибочка:",sys.exc_info()[0])
+#
+#
+#
+# #
+# # raise KeyboardInterrupt
+#
+#
+# # try:
+# #     print("hello")
+# # except:
+# #     print("что то не так")
+# # else:
+# #     print("все работает как надо")
+#
+#
+# # try :
+# #     print("hey")
+# # except:
+# #     print("что то пошло не так")
+# # else:
+# #     print("все по плану")
+# # finally:
+# #     print("ok")
+#
+# # def adder(x,y,z):
+# #     print("sum:",x+y+z)
+# #
+# # adder(10,12,13)
+#
+#
+#
+# # def adder(*num):
+# #     summari = 0
+# #
+# #     for n in num:
+# #        summari = summari + n
+# #     print("Sum:",summari)
+# #
+# # adder(5,4)
+# # adder(5,8,4)
+# # adder(2,5,7,3)
+#
+#
+#
+# #
+# # def intro(**date):
+# #     print("\nDate type of argument:",type(date))
+# #
+# #     for key, value in date.items():
+# #         print("() is ()".format(key,value))
+# #
+# #
+# # intro(Firstname="ermek", Lastname="Sharma", Age=20)
+# # intro(Firstname="eliza", Lastname="wood",Age=18)
+#
+#
+#
+# def maximum(x,y,z):
+#   if x>y and x>z:
+#     return x
+#   elif y>x and y>z:
+#     return y
+#   else:
+#     return z
+# print(maximum(1,2,3))
+
+#
+# def max1(*numbers):
+#     var = 0
+#     for num in numbers:
+#         if num>var:
+#             var = num
+#     return var
+# print(max1(1,3,4,5,7,58,655,5466,5566))
+#
+#
+#
+# def operator(num1, num2):
+#     print(num1+num2)
+#     print(num1-num2)
+# operator(4,3)
+
+#
+# my_list=[1,2,3,4,5]
+#  try:
+#      print(my_list[6])
+#  except:IndexError
+#      print()
+
+
+# num1 = 14
+# num2 = "z"
+# try:
+#     print(num1+num2)
+# except BaseException:
+#     print("ошибочка")
+
+
+#
+# inport random
+# num=random.randint(0,10)
+#
+# gvess =int(input("введите число от 0 до 10"))
+# while True:
+#     if gvess < 0 or gvess >10:
+#         print("введите число из тогоб что мы загадали")
+#         gvess = int(input("введите число от 0 до 10"))
+#     else:
+#         if gvess <num:
+#             print("мало")
+#             gvess = int(input("введите число от 0 до 10"))
+#         elif gvess > num:
+#             print
+
+# import random
+# choices =["камен","ножница","бумага"]
+# print("каменб > ножница, ножница > бумага, бумага>камен")
+# player = input("выберите камен , ножниса ,бумага или выйти: ")
+# while player != "выйти":
+#     player = player.lower()
+#     computer = random.choice(choices)
+#     print ("вы выбрали", player, "компютер выбрал",computer )
+#     if player==computer:
+#         print("ничя")
+#     elif player== "камень":
+#         if computer == "ножницы":
+#             print("вы выйграли")
+#         else:
+#             print("вы проиграли")
+#     elif player== "бумага":
+#         if computer == "камен":
+#             print("вы выйграли")
+#         else:
+#             print("вы програли")
+#     elif player == "ножницы":
+#         if computer == "бумага":
+#             print("вы выйграли")
+#         else:
+#             print("вы програли")
+#     else:
+#         print("произошла какая то ошибка")
+#     print()
+#     player = input("выберите камен , ножниса ,бумага или выйти:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
